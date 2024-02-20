ac7569c959c837f66ee0d3587cf682e5d86c3d2d
rahuljat1349 <rahuldudi1349@gmail.com>
2024-02-20 10:39:45 +0530

    modified search box

--- src/Components/Layout/SearchBox/Search.jsx
+++ src/Components/Layout/SearchBox/Search.jsx
@@ -14,24 +14,24 @@ export default function Search() {
   };
   return (
     <>
-      <h1 className=" w-full bg-gray-700 text-gray-300 text-2xl p-4 text-center">
+      <h1 className=" w-full bg-gray-800 text-gray-300 text-2xl pt-20 text-center">
         {" "}
         Search Products
       </h1>
 
-      <div className="w-full shadow-xl shadow-black bg-gray-700 h-[90vh] flex justify-center ">
-        <div className="mt-36 rounded bg-violet-200 h-10 overflow-hidden">
+      <div className="w-full shadow-xl shadow-black bg-gray-800 h-[90vh] flex justify-center ">
+        <div className="mt-20 flex justify-center rounded-lg bg-red-600 w-[60%] h-14 overflow-hidden">
           <input
             placeholder="Search for a product.."
             onChange={(e) => {
               setKeyword(e.target.value);
             }}
-            className="p-2 outline-none placeholder:text-gray-500 bg-gray-400 text-gray-200 w-64"
+            className="p-6  outline-none placeholder:text-gray-500 bg-gray-700 text-gray-200 w-full"
             type="text"
           />
           <button
             onClick={handleSearchSubmit}
-            className="p-2 hover:bg-red-500 duration-200 active:bg-red-600 bg-red-400 text-white"
+            className="p-4 hover:bg-red-600 duration-200 active:bg-red-700 bg-red-500 text-white"
           >
             Search
           </button>
